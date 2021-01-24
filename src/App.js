4400d6e4ddb29d7594a4f3c1d85184117843bb52
Raghu <raghunath@adrobit.com>
2021-01-24 10:43:29 +0530

    logic fix

--- src/App.js
+++ src/App.js
@@ -13,19 +13,67 @@ const itemArray = new Array(9).fill("empty");
 const App = () => {
 
   const [isCross, setIsCross] = useState(false);
-  const [winMessages, setWinMessages] = useState("");
+  const [winMessages, setWinMessage] = useState("");
 
   const reloadGame = () => {
     setIsCross(false);
-    setWinMessages("");
+    setWinMessage("");
     itemArray.fill('empty', 0, 9);
   };
 
   const checkIsWiner = () => {
-    if (itemArray[0] === itemArray[1] && itemArray[0] === itemArray[2] && itemArray[0] !== 'empty') {
-      setWinMessages(`${itemArray[0]} wins`);
+    //  checking  winner of the game
+    if (
+      itemArray[0] === itemArray[1] &&
+      itemArray[0] === itemArray[2] &&
+      itemArray[0] !== "empty"
+    ) {
+      setWinMessage(`${itemArray[0]} won`);
+    } else if (
+      itemArray[3] !== "empty" &&
+      itemArray[3] === itemArray[4] &&
+      itemArray[4] === itemArray[5]
+    ) {
+      setWinMessage(`${itemArray[3]} won`);
+    } else if (
+      itemArray[6] !== "empty" &&
+      itemArray[6] === itemArray[7] &&
+      itemArray[7] === itemArray[8]
+    ) {
+      setWinMessage(`${itemArray[6]} won`);
+    } else if (
+      itemArray[0] !== "empty" &&
+      itemArray[0] === itemArray[3] &&
+      itemArray[3] === itemArray[6]
+    ) {
+      setWinMessage(`${itemArray[0]} won`);
+    } else if (
+      itemArray[1] !== "empty" &&
+      itemArray[1] === itemArray[4] &&
+      itemArray[4] === itemArray[7]
+    ) {
+      setWinMessage(`${itemArray[1]} won`);
+    } else if (
+      itemArray[2] !== "empty" &&
+      itemArray[2] === itemArray[5] &&
+      itemArray[5] === itemArray[8]
+    ) {
+      setWinMessage(`${itemArray[2]} won`);
+    } else if (
+      itemArray[0] !== "empty" &&
+      itemArray[0] === itemArray[4] &&
+      itemArray[4] === itemArray[8]
+    ) {
+      setWinMessage(`${itemArray[0]} won`);
+    } else if (
+      itemArray[2] !== "empty" &&
+      itemArray[2] === itemArray[4] &&
+      itemArray[4] === itemArray[6]
+    ) {
+      setWinMessage(`${itemArray[2]} won`);
     }
   };
+
 
   const changeItem = itemNumber => {
     if (winMessages) {
